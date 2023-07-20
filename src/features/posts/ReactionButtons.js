3def8921a37d8d66b7db2b87d285a061d48fd50d
it-dude-ru <it-dude@ya.ru>
2023-07-20 21:44:43 +0300

    Reaction buttons added

--- src/features/posts/ReactionButtons.js
+++ src/features/posts/ReactionButtons.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { reactionAdded } from "./postsSlice";
 
 const reactionEmoji = {
 	thumbsUp: '👍',
@@ -8,10 +10,19 @@ const reactionEmoji = {
 	eyes: '👀'
 }
 
-export const ReactionButtons = ({post}) => {
+export const ReactionButtons = ({ post }) => {
+	const dispatch = useDispatch();
+
 	const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
 		return (
-			<button key={name} type="button" className="muted-button reaction-button">
+			<button
+				key={name}
+				type="button"
+				className="muted-button reaction-button"
+				onClick={() =>
+					dispatch(reactionAdded({ postId: post.id, reaction: name }))
+				}
+			>
 				{emoji} {post.reactions[name]}
 			</button>
 		)
