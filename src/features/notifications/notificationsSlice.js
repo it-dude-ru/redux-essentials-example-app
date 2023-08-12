3f12008460cd95c466e45ab77186da0442417ce6
it-dude-ru <it-dude@ya.ru>
2023-08-12 22:36:01 +0300

    Sowing new notifications

--- src/features/notifications/notificationsSlice.js
+++ src/features/notifications/notificationsSlice.js
@@ -18,15 +18,26 @@ export const fetchNotifications = createAsyncThunk(
 const notificationsSlice = createSlice({
 	name: 'notifications',
 	initialState: [],
-	reducers: {},
+	reducers: {
+		allNotificationsRead(state, action) {
+			state.forEach(notification => {
+				notification.read = true;
+			});
+		}
+	},
 	extraReducers(builder) {
 		builder.addCase(fetchNotifications.fulfilled, (state, action) => {
 			state.push(...action.payload);
+			state.forEach(notification => {
+				notification.isNew = !notification.read;
+			});
 			// Sort with newest first
 			state.sort((a, b) => b.date.localeCompare(a.date));
 		})
 	}
 });
+
+export const { allNotificationsRead } = notificationsSlice.actions;
 
 export default notificationsSlice.reducer;
 
