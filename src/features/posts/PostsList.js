727b53969eecf5b6ce35dc858017ad28b0729db7
it-dude-ru <it-dude@ya.ru>
2023-08-19 14:12:55 +0300

    Post List optimized with normalized state

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -6,9 +6,10 @@ import { Spinner } from '../../components/Spinner';
 import { PostAuthor } from './PostAuthor';
 import { TimeAgo } from './TimeAgo';
 import { ReactionButtons } from './ReactionButtons';
-import { fetchPosts, selectAllPosts } from './postsSlice';
+import { fetchPosts, selectAllPosts, selectPostById, selectPostIds } from './postsSlice';
 
-const PostExcerpt = ({ post }) => {
+const PostExcerpt = ({ postId }) => {
+	const post = useSelector(state => selectPostById(state, postId));
 	return (
 		<article className="post-excerpt">
 			<h3>{post.title}</h3>
@@ -27,7 +28,7 @@ const PostExcerpt = ({ post }) => {
 
 export const PostsList = () => {
 	const dispatch = useDispatch();
-	const posts = useSelector(selectAllPosts);
+	const orderedPostIds = useSelector(selectPostIds);
 
 	const postStatus = useSelector(state => state.posts.status);
 	const error = useSelector(state => state.posts.error);
@@ -41,9 +42,8 @@ export const PostsList = () => {
 	if (postStatus === 'loading') {
 		content = <Spinner text="Loading..." />
 	} else if (postStatus === 'succeeded') {
-		const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
-		content = orderedPosts.map(post => (
-			<PostExcerpt key={post.id} post={post} />
+		content = orderedPostIds.map(postId => (
+			<PostExcerpt key={postId} post={postId} />
 		))
 	} else if (postStatus === 'failed') {
 		content = <div>{error}</div>
