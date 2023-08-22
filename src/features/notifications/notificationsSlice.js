137ac42d0be3e7afce05487310cf100642c7014f
it-dude-ru <it-dude@ya.ru>
2023-08-22 17:49:32 +0300

    Converting Other Slices

--- src/features/notifications/notificationsSlice.js
+++ src/features/notifications/notificationsSlice.js
@@ -1,6 +1,10 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createEntityAdapter, createSlice } from '@reduxjs/toolkit';
 
 import { client } from '../../api/client';
+
+const notificationsAdapter = createEntityAdapter({
+	sortComparer: (a, b) => b.date.localeCompare(a.date)
+});
 
 export const fetchNotifications = createAsyncThunk(
 	'notifications/fetchNotifications',
@@ -17,22 +21,20 @@ export const fetchNotifications = createAsyncThunk(
 
 const notificationsSlice = createSlice({
 	name: 'notifications',
-	initialState: [],
+	initialState: notificationsAdapter.getInitialState(),
 	reducers: {
 		allNotificationsRead(state, action) {
-			state.forEach(notification => {
+			Object.values(state.entities).forEach(notification => {
 				notification.read = true;
-			});
+			})
 		}
 	},
 	extraReducers(builder) {
 		builder.addCase(fetchNotifications.fulfilled, (state, action) => {
-			state.push(...action.payload);
-			state.forEach(notification => {
+			notificationsAdapter.upsertMany(state, action.payload);
+			Object.values(state.entities).forEach(notification => {
 				notification.isNew = !notification.read;
 			});
-			// Sort with newest first
-			state.sort((a, b) => b.date.localeCompare(a.date));
 		})
 	}
 });
@@ -41,4 +43,5 @@ export const { allNotificationsRead } = notificationsSlice.actions;
 
 export default notificationsSlice.reducer;
 
-export const selectAllNotifications = state => state.notifications;
+export const { selectAll: selectAllNotifications } =
+	notificationsAdapter.getSelectors(state => state.notifications);
